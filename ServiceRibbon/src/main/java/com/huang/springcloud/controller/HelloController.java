c3b4e26284b8da926f6fe4acdf1b166056c225c8
huangshizeng <shizeng101@163.com>
2018-08-02 11:15:40 +0800

    添加了分布式配置中心

--- ServiceRibbon/src/main/java/com/huang/springcloud/controller/HelloController.java
+++ ServiceRibbon/src/main/java/com/huang/springcloud/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.huang.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,15 @@ import com.huang.springcloud.service.HelloService;
 @RestController
 public class HelloController {
 	
-	 @Autowired
-	 private HelloService helloService;
-	 
-	 @GetMapping("/hi")
-	 public String hello(@RequestParam("name") String name) {
-		 System.out.println("ServiceRibbon.");
-		 return helloService.helloService(name);
-	 }
+	@Autowired
+	private HelloService helloService;
+	@Value("${aaa}")
+	String aaa;
+
+	@GetMapping("/hi")
+	public String hello(@RequestParam("name") String name) {
+		System.out.println("ServiceRibbon.");
+		System.out.println("配置文件中的aaa=" + aaa);
+	    return helloService.helloService(name);
+	}
 }
