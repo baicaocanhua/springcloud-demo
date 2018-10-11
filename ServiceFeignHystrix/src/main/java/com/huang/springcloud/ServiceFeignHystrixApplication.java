adb0e8e0d804bbcd60ee05e110c2487b521732ea
huangshizeng <shizeng101@163.com>
2018-10-11 10:53:04 +0800

    添加Hystrix Dashboard工程

--- ServiceFeignHystrix/src/main/java/com/huang/springcloud/ServiceFeignHystrixApplication.java
+++ ServiceFeignHystrix/src/main/java/com/huang/springcloud/ServiceFeignHystrixApplication.java
@@ -2,17 +2,14 @@ package com.huang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 public class ServiceFeignHystrixApplication {
 
 	public static void main(String[] args) {
